1e048566701601903aa1ca09701f26fc9bd5d050
Patrick Kerwood <patrick@kerwood.dk>
2022-11-07 21:23:38 +0100

    Updated nvim-treesitter config

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -4,13 +4,14 @@ if not status_ok then
 end
 
 configs.setup({
-	ensure_installed = "all", -- one of "all" or a list of languages
-	ignore_install = { "" }, -- List of parsers to ignore installing
-	highlight = {
-		enable = true, -- false will disable the whole extension
-		disable = { "" }, -- list of language that will be disabled
-	},
-	indent = { enable = true },
+  ensure_installed = "all", -- one of "all" or a list of languages
+  ignore_install = { "" }, -- List of parsers to ignore installing
+  highlight = {
+    enable = true, -- false will disable the whole extension
+    additional_vim_regex_highlighting = false,
+    disable = { "" }, -- list of language that will be disabled
+  },
+  indent = { enable = true },
   rainbow = {
     enable = true,
     extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
